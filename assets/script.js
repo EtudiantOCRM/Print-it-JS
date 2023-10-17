bf4e412fc23f3c19f2137f1418d14987d63be0e9
Rachida Mohamed <rachidamohamed@yahoo.fr>
2023-10-17 14:08:24 +0200

    Mise en place du Slider

--- assets/script.js
+++ assets/script.js
@@ -17,10 +17,10 @@ const slides = [
 	}
 ]
 
+//Gestion de la flèche gauche
+
 let counter = 0
 
-//console.log(slides)
-//console.log(slides[0].tagLine)
 const arrow_left = document.querySelector(`.arrow_left`)
 arrow_left.addEventListener(`click`, () => {
 	console.log("j'ai cliqué sur la flèche gauche")
@@ -29,61 +29,71 @@ arrow_left.addEventListener(`click`, () => {
 	if (counter < 0) {
 		counter = slides.length - 1
 	}
-	//let htmlImage = `<img src="images/slideshow/${slides[counter]}" alt="">`
-	//element.innerHTML = htmlImage
+	
 	const baliseImage = document.querySelector(".banner-img");
 
 	console.log(counter)
 	console.log("image dans le slide[counter]", slides[counter].image)
 
-
 	baliseImage.src = `./assets/images/slideshow/${slides[counter].image}`;
-
-	//baliseImage.setAttribute("src", "./assets/images/slideshow/"+ element.image)
-	//element.innerHTML=tagLine;
-	//const baliseTexte = document.querySelector("p");
 
 	tagLine = slides[counter].tagLine;
 	document.querySelector(`p`).innerHTML = tagLine;
-
+	gererPoint(counter)
 })
-
 console.log(arrow_left)
+
+
+//Gestion de la flèche droite
 
 const arrow_right = document.querySelector(`.arrow_right`)
 arrow_right.addEventListener(`click`, () => {
 	console.log("j'ai cliqué sur la flèche droite")
 	counter = counter + 1
 	console.log(counter, slides[counter])
-	if (counter > slides.length){
+	if (counter >= slides.length) {
 		counter = 0
 	}
 	let baliseImage = document.querySelector(".banner-img")
-
 
 	baliseImage.src = `./assets/images/slideshow/${slides[counter].image}`;
 
 	tagLine = slides[counter].tagLine;
 	document.querySelector(`p`).innerHTML = tagLine;
 
-	
-
+	gererPoint(counter)
 })
 console.log(arrow_right)
 
 
-
-
+//gestion des bullets points
 
 let dots = document.querySelector(`.dots`)
 let dot = ""
+
+function gererPoint(numeroPointAallumer) {
+	
+	let listePoints = document.querySelectorAll(".dot")
+	//console.log(listePoints)
+	for (let i = 0; i < listePoints.length; i++) {
+
+		if (i == numeroPointAallumer) {
+			//console.log("j'allume le point ", i)
+			listePoints[i].classList.add("dot_selected")
+		} else {
+			//console.log("j'éteins le point", i)
+			listePoints[i].classList.remove("dot_selected")
+		}
+	}
+}
 
 for (let i = 0; i < slides.length; i++) {
 	if (i == 0) {
 		dot = `<div class="dot dot_selected"></div>`
 	}
 	else {
-		dot = `<div class="dot" id="dot-${i}"></div>`
+		dot = `<div class="dot"></div>`
 	}
 	dots.innerHTML = dots.innerHTML + dot
 }
+
